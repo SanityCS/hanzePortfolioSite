50dde5d9d661558cd5da7e2881812337c075bb87
Sanity <evandokter@gmail.com>
2024-02-13 23:16:24 +0100

    it's starting to get somewhere

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, flash, redirect
+from flask import Flask, render_template, request, url_for, flash, redirect, abort
 import sqlite3
 
 app = Flask(__name__)
@@ -8,6 +8,15 @@ def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
 
 @app.route('/')
 def index():
@@ -38,7 +47,7 @@ def network_notes():
 def infrastructure_notes():
     return render_template('infrastructure_notes.html')
 
-@app.route('/create')
+@app.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
         title = request.form['title']
@@ -57,4 +66,38 @@ def create():
             return redirect(url_for('posts'))
 
     return render_template('create.html')
+
+@app.route('/<int:id>/edit/', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('A post title is required!')
+        
+        elif not content:
+            flash('Some content for the post is required!')
+
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ?'
+                         ' WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('posts'))
+    return render_template('edit.html', post=post)
+
+@app.route('/<int:id>/delete/', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted from the posts!'.format(post['title']))
+    return redirect(url_for('posts'))
 app.run(debug=True)
