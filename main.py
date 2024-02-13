55d5738e6a5feaba94953cad4bc52389776456ca
Sanity <evandokter@gmail.com>
2024-02-13 21:44:11 +0100

    lets test if this works on all systems

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
+import sqlite3
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ea579ce9def1ffca2d3dfd78a6aa9c9760b270950268405b'
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 @app.route('/')
 def index():
@@ -15,9 +22,12 @@ def projects():
     return render_template('projects.html')
 
 
-@app.route('/notes')
-def notes():
-    return render_template('notes.html')
+@app.route('/posts', methods=['GET', 'POST'])
+def posts():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('posts.html', posts=posts)
 
 
 @app.route('/network_notes')
@@ -28,4 +38,23 @@ def network_notes():
 def infrastructure_notes():
     return render_template('infrastructure_notes.html')
 
+@app.route('/create')
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('A post title is required!')
+        elif not content:
+            flash('Some content for the post is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('posts'))
+
+    return render_template('create.html')
 app.run(debug=True)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./templates/*.html"],
   theme: {
     extend: {},
+    colors: {
+      black: colors.black,
+      purple: colors.purple,
+      gray: colors.gray,
+      white: colors.white,
+      indigo: colors.indigo,
+      red: colors.red,
+      teal: colors.teal,
+      yellow: colors.yellow,
+      blue: colors.blue,
+
+    },
   },
   plugins: [],
 }
